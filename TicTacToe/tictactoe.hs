93d0f5caf9d90bdc111d20a3b42f270f3d20fa9c
Andrew Ribeiro <andrewnetwork@gmail.com>
2018-12-03 19:41:26 -0500

    Game enumeration

--- TicTacToe/tictactoe.hs
+++ TicTacToe/tictactoe.hs
@@ -2,6 +2,7 @@
 -- December 2018
 -- Andrew Ribeiro 
 
+module TicTacToe where
 -- Rules of the game: 
 -- > Board is initially empty.
 -- > A starting player is chosen at random. 
@@ -14,13 +15,21 @@
 import Data.List.Split
 import Data.List (transpose,elemIndex)
 import Data.Maybe
+import Data.Monoid
+import System.Random
+
+-- #### Helper Functions ####
+printBoard :: Board -> IO ()
+printBoard boardState = putStrLn.init $ concatMap (\x->x++"\n") (map show boardState)
+-- printBoard [[(P X),E,E],[E,(P X),E],[E,E,(P X)]]
 
 -- #### Data Structures ####
 -- Cells, Boards, Turns, Games. 
 -- Boards ⊂ Cell X Cell X ..6.. X Cell 
 data Player = X | O deriving Show 
-data Cell = E | P Player deriving Show 
-data EndState = D | W Player deriving Show 
+data Cell = E | P Player
+data EndState = D | W Player deriving (Show,Eq)
+type Move = (Int,Int)
 type Board = [[Cell]]
 
 instance Eq Cell where
@@ -28,6 +37,10 @@ instance Eq Cell where
     P a == P b = a == b
     E == P a = False 
     P a == E = False   
+
+instance Show Cell where 
+    show E = "E"
+    show (P x) = show x
 
 instance Eq Player where
     X == O = False 
@@ -41,8 +54,7 @@ initialState = [[E,E,E],[E,E,E],[E,E,E]]
 -- (2) [[E,E,E],[E,E,E],[E,E,E]]
 -- We will be using the nested representation (2).  
 
--- #### Functions ####
-
+-- #### Game Functions ####
 insert :: (Eq t1, Num t1) => t2 -> [t2] -> t1 -> [t2]
 insert e [] pos = []
 insert e (x:xs) pos
@@ -50,11 +62,77 @@ insert e (x:xs) pos
     | otherwise = x:(insert e (xs) (pos-1))
 -- insert 2 [1,1,1,1,1,1] 3 
 
-move :: Cell -> [Cell] -> Int -> [Cell]
-move player boardState position = insert player boardState position
--- move (P X) initialState 2
--- move (P O) (move (P X) initialState 2) 0
- 
+collapseIndex :: Num a => (a, a) -> a -> a
+collapseIndex multIndex rowLen = (fst multIndex)*rowLen+(snd multIndex)
+
+move :: Player -> Board -> Move -> Board
+move player boardState position = chunksOf 3 (insert 
+                                                (playerToCell player) 
+                                                (mconcat boardState) 
+                                                (collapseIndex position 3))
+-- move X initialState (2,2)
+-- printBoard (move X initialState (2,2))
+-- printBoard (move O (move X initialState (2,1)) (0,0))
+-- printBoard (move O (move O initialState (2,1)) (0,0))
+
+-- We need to account for three conditions in determining if a move is valid. 
+-- (1) The number of player pieces differs by no more than one. (captures alternating turn rule)
+-- (2) The game is not in a terminal state. (not a win/draw already)
+-- (3) A player piece can only replace an empty cell, not another player. (no obstructions)
+
+isValidMove :: Player -> Board -> Move -> Bool
+isValidMove player boardState position = foldl (&&) True (validateMove player boardState position)
+
+validateMove :: Player -> Board -> Move -> [Bool]
+validateMove player boardState position = [pieceCountCond,terminalCond,emptyCellCond]
+                                          where proposedBoard = move player boardState position
+                                                pieceCountCond = abs((countPlayer player boardState)+1 - (countPlayer (otherPlayer player) boardState)) <= 1
+                                                terminalCond = not (isTerminalState boardState) 
+                                                emptyCellCond = not (isPlayerLoc boardState position)
+-- validateMove O initialState (0,0)
+speakAboutMove :: Player -> Board -> Move -> [Char]
+speakAboutMove player boardState position = (if not (validationConds !! 0) then "It's the other player's turn! " else "")++
+                                            (if not (validationConds !! 1) then "The game is over man! " else "")++
+                                            (if not (validationConds !! 2) then "There's a piece there! " else "")
+                                            where validationConds = validateMove player boardState position
+-- speakAboutMove O initialState (0,0)
+-- speakAboutMove O [[(P O),E,E],[E,E,E],[E,E,E]] (0,0)
+-- speakAboutMove O [[(P O),E,E],[E,(P O),E],[E,E,(P O)]] (2,2)
+
+
+--playerTurn boardState =
+otherPlayer X = O
+otherPlayer O = X
+
+-- [isPieceCountValid ,isTerminalState,isPlayerLoc]
+
+isTerminalState :: Board -> Bool
+isTerminalState boardState = case (terminalState boardState) of
+                                Nothing -> False 
+                                _ -> True
+-- isTerminalState [[(P O),E,E],[E,(P O),E],[E,E,(P X)]]
+-- isTerminalState [[(P O),E,E],[E,(P O),E],[E,E,(P O)]]
+
+isPlayerLoc :: Board -> Move -> Bool
+isPlayerLoc boardState move = ((concat boardState)!!(collapseIndex move 3)) /= E
+--isPlayerLoc [[(P O),E,E],[E,(P O),E],[E,E,(P X)]] (0,0)
+
+isPieceCountValid :: Board -> Bool
+isPieceCountValid boardState = (abs ((countPlayer X boardState)-(countPlayer O boardState))) <= 1
+-- isPieceCountValid [[(P X),E,E],[E,(P X),E],[E,E,(P X)]] -> False
+-- isPieceCountValid initialState
+-- isPieceCountValid [[(P O),E,E],[E,(P O),E],[E,E,(P X)]] 
+
+countCell :: Cell -> Board -> Int
+countCell cell boardState = length $ filter (cell==) (concat boardState) 
+-- countCell (P X) [[(P X),E,E],[E,(P X),E],[E,E,(P X)]]
+
+countPlayer :: Player -> Board -> Int
+countPlayer player boardState = countCell (playerToCell player) boardState
+-- countPlayer X [[(P X),E,E],[E,(P X),E],[E,E,(P X)]]
+
+-- validMoves player boardState = map (\x-> move player boardState) [(x,y) | x<-[0,1,2],y<-[0,1,2]]
+
 -- [[0,1,2],[3,4,5],[6,7,8]] -> [0,4,8]
 -- [[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15]] -> [0,5,10,15]
 -- Problem: Given a list of lists, produce a list where the first element 
@@ -71,6 +149,10 @@ isAllPlayer [a,b,c] = (a == b) && (b == c) && a /= E
 cellToPlayer :: Cell -> Player
 cellToPlayer (P x) = x
 cellToPlayer E = error "Cannot convert E to a player."
+
+playerToCell :: Player -> Cell
+playerToCell X = P X
+playerToCell O = P O  
 
 -- #### Win Conditions ####
 whoWonDiag :: Board -> Maybe Player
@@ -119,6 +201,51 @@ terminalState boardState
 -- terminalState [[(P X),E,E],[E,(P X),E],[E,E,(P X)]]
 -- terminalState initialState 
 
--- #### Printing Functions ####
-printBoard boardState = concatMap (\x->x++"\n") (map show boardState)
--- printBoard [[(P X),E,E],[E,(P X),E],[E,E,(P X)]]
+validMoves :: Player -> Board -> [Move]
+validMoves player boardState = [(x,y) | x<-[0,1,2], y<-[0,1,2], isValidMove player boardState (x,y)]
+-- validMoves O initialState
+
+--makeRandomMove player boardState  = do let moves = validMoves player boardState
+                                     -- idx <- randomRIO(0,length(moves))
+                                      --move player boardState (moves !! idx) 
+
+                                      
+pickFirstMove player boardState = move player boardState (head (validMoves player boardState))
+-- pickFirstMove X (pickFirstMove O initialState)
+-- pickFirstMove O (pickFirstMove O initialState)
+
+--testBank = do x <- traverse (\z -> randomRIO(1::Int,z)) [1,2,3]
+--return x        
+                  
+playGame initPlayer boardState = case (terminalState boardState) of
+                                    Nothing -> newBoard:(playGame (otherPlayer initPlayer) newBoard)
+                                    _ -> []
+                                    where newBoard = (pickFirstMove initPlayer boardState)
+
+-- terminalState (last (playGame O initialState))
+
+-- [[O,X,O], 
+--  [X,O,X],
+--  [O,E,E]]
+ -- pickFirstMove O (pickFirstMove X (pickFirstMove O (pickFirstMove X (pickFirstMove O (pickFirstMove X (pickFirstMove O initialState))))))
+ -- validMoves X (pickFirstMove O (pickFirstMove X (pickFirstMove O initialState)))
+ -- validMoves O (pickFirstMove X (pickFirstMove O (pickFirstMove X (pickFirstMove O (pickFirstMove X (pickFirstMove O initialState))))))
+ -- validMoves O [[(P O),(P X),(P O)],[(P X),(P O),(P X)],[E,E,E]]
+ -- speakAboutMove O [[(P O),(P X),(P O)],[(P X),(P O),(P X)],[E,E,E]] (2,0)
+ -- terminalState [[(P O),(P X),(P O)],[(P X),(P O),(P X)],[E,E,E]]
+ -- validateMove O [[(P O),(P X),(P O)],[(P X),(P O),(P X)],[E,E,E]] (2,0)
+
+gameOutComes :: Player -> Board -> [EndState]
+gameOutComes player [] = [] 
+gameOutComes player boardState 
+    | isTerminalState boardState = maybeToList $ terminalState boardState -- Maybe EndState 
+    | otherwise = concatMap (gameOutComes (otherPlayer player)) boards -- []
+    where boards = makeAllMoves player boardState
+
+-- x = gameOutComes O initialState 
+
+makeAllMoves :: Player -> Board -> [Board]
+makeAllMoves player boardState = (map (move player boardState) (validMoves player boardState))
+
+--map (\x -> move O initialState x) (validMoves O initialState)
+--
